1c8b10455b99e90a3e18953abb6f6f89ad4ff8b5
Augusto Salazar SIlva <augustosalazar@uninorte.edu.co>
2024-10-04 10:53:57 -0500

    Update Componente.java

--- src/main/java/com/uninorte/Componente.java
+++ src/main/java/com/uninorte/Componente.java
@@ -1,19 +1,5 @@
 package com.uninorte;
 
 public class Componente {
-    private int id;
-    private Avion avion;
-
-    public Componente(int id, Avion avion) {
-        this.id = id;
-        this.avion = avion;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public Avion getAvion() {
-        return avion;
-    }
+   
 }
