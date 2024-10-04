c5daea336bf3f2627f3ba364f9d8904da0471a6a
Augusto Salazar SIlva <augustosalazar@uninorte.edu.co>
2024-10-04 10:53:41 -0500

    Update Avion.java

--- src/main/java/com/uninorte/Avion.java
+++ src/main/java/com/uninorte/Avion.java
@@ -4,30 +4,6 @@ import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 public class Avion {
-    private int id;
-    private Equipo equipo;
-    private ArrayList<Componente> componentes;
-
-    public Avion(int id, Equipo equipo) {
-        this.id = id;
-        this.equipo = equipo;
-        this.componentes = new ArrayList<Componente>();
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public Equipo getEquipo() {
-        return equipo;
-    }
-
-    public void addComponente(int id) {
-        this.componentes.add(new Componente(id, this));
-    }
-
-    public int getNumeroComponentes() {
-        return this.componentes.size();
-    }
+  
     
 }
